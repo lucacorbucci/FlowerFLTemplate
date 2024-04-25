9f0fc4d88e0bcaaaf166177b6e084bd58c0a271f
lucacorbucci <corbuccilu@gmail.com>
2024-04-25 12:27:38 +0200

    Updated prefeernces

--- fltemplate/Utils/preferences.py
+++ fltemplate/Utils/preferences.py
@@ -10,17 +10,16 @@ class Preferences:
     dataset_path: str
     # The number of epochs that we want to use during the training
     epochs: int
-    # device on which we want to train the model
-    device: torch.device
     batch_size: int
     # seed to use during the training
     seed: int
     optimizer: str
+    learning_rate: float
     # regularization is True if we want to use the regularization to
     # reduce the unfairness of the model during the training
     epsilon: float = None
     sweep: bool = False
-    fl_round: int = None
+    fl_rounds: int = None
     noise_multiplier: float = None
     cross_device: bool = False
     tabular: bool = False
@@ -28,6 +27,7 @@ class Preferences:
     split_approach: str = None
     alpha_dirichlet: float = None
     ratio_unfair_nodes: float = None
-    group_to_reduce: tuple = None 
+    group_to_reduce: tuple = None
     group_to_increment: tuple = None
     ratio_unfairness: tuple = None
+    validation: bool = False
