6d4129e6de52421c63b31017307fbbe766b5d34f
lucacorbucci <corbuccilu@gmail.com>
2024-04-25 12:27:30 +0200

    Added aggregation functions in a separate utils file

---
+++ fltemplate/Utils/aggregation.py
@@ -0,0 +1,122 @@
+import logging
+from logging import INFO
+
+import numpy as np
+import wandb
+from flwr.common.logger import log
+
+
+class Aggregation:
+    def agg_metrics_test(metrics: list, server_round: int, wandb_run) -> dict:
+        total_examples = sum([n_examples for n_examples, _ in metrics])
+
+        loss_test = (
+            sum([n_examples * metric["loss"] for n_examples, metric in metrics])
+            / total_examples
+        )
+        accuracy_test = (
+            sum([n_examples * metric["accuracy"] for n_examples, metric in metrics])
+            / total_examples
+        )
+        f1_test = (
+            sum([n_examples * metric["f1_score"] for n_examples, metric in metrics])
+            / total_examples
+        )
+
+        log(
+            INFO,
+            f"Test Accuracy: {accuracy_test} - Test Loss {loss_test}",
+        )
+
+        agg_metrics = {
+            "Test Loss": loss_test,
+            "Test Accuracy": accuracy_test,
+            "FL Round": server_round,
+            "Test F1": f1_test,
+        }
+        if wandb_run:
+            wandb_run.log(agg_metrics)
+
+        return agg_metrics
+
+    def agg_metrics_evaluation(metrics: list, server_round: int, wandb_run) -> dict:
+        total_examples = sum([n_examples for n_examples, _ in metrics])
+
+        loss_evaluation = (
+            sum([n_examples * metric["loss"] for n_examples, metric in metrics])
+            / total_examples
+        )
+        accuracy_evaluation = (
+            sum([n_examples * metric["accuracy"] for n_examples, metric in metrics])
+            / total_examples
+        )
+        f1_validation = (
+            sum([n_examples * metric["f1_score"] for n_examples, metric in metrics])
+            / total_examples
+        )
+
+        agg_metrics = {
+            "Validation Loss": loss_evaluation,
+            "Validation_Accuracy": accuracy_evaluation,
+            "FL Round": server_round,
+            "Validation F1": f1_validation,
+        }
+        if wandb_run:
+            wandb_run.log(agg_metrics)
+
+        return agg_metrics
+
+    def agg_metrics_train(
+        metrics: list, server_round: int, current_max_epsilon: float, fed_dir, wandb_run
+    ) -> dict:
+        # Collect the losses logged during each epoch in each client
+        total_examples = sum([n_examples for n_examples, _ in metrics])
+        analysis_dicts = []
+        losses = []
+        losses_with_regularization = []
+        epsilon_list = []
+        accuracies = []
+        max_disparity_train = []
+
+        total_analysis_dict = {}
+
+        for n_examples, node_metrics in metrics:
+            losses.append(n_examples * node_metrics["train_loss"])
+            epsilon_list.append(node_metrics["epsilon"])
+            accuracies.append(n_examples * node_metrics["train_accuracy"])
+            client_id = node_metrics["cid"]
+
+            # Create the dictionary we want to log. For some metrics we want to log
+            # we have to check if they are present or not.
+            to_be_logged = {
+                "FL Round": server_round,
+            }
+
+            delta = node_metrics["delta"]
+            if delta:
+                to_be_logged[f"Delta Client {client_id}"] = delta
+
+            if wandb_run:
+                wandb_run.log(
+                    to_be_logged,
+                )
+
+        log(
+            INFO,
+            f"Train Accuracy: {sum(accuracies) / total_examples} - Train Loss {sum(losses) / total_examples}",
+        )
+
+        agg_metrics = {
+            "Train Loss": sum(losses) / total_examples,
+            "Train Accuracy": sum(accuracies) / total_examples,
+            "Train Loss with Regularization": sum(losses_with_regularization)
+            / total_examples,
+            "Training Disparity with average": sum(max_disparity_train)
+            / len(max_disparity_train),
+            "Train Epsilon": current_max_epsilon,
+            "FL Round": server_round,
+        }
+        if wandb_run:
+            wandb_run.log(agg_metrics)
+
+        return agg_metrics
