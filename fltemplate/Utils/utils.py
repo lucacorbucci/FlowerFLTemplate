95a9a785d0df64de90c02e5d567796f17ed3a756
lucacorbucci <corbuccilu@gmail.com>
2024-04-25 12:27:55 +0200

    Started the implementation of a Util class

---
+++ fltemplate/Utils/utils.py
@@ -0,0 +1,152 @@
+import os
+import random
+from typing import Any, Callable, List, Optional, Tuple
+
+import numpy as np
+import torch
+from federated_baseline.Models.celeba import CelebaNet
+from federated_baseline.Models.logistic_regression_net import LinearClassificationNet
+from opacus import PrivacyEngine
+from opacus.grad_sample import GradSampleModule
+from opacus.optimizers import DPOptimizer
+from torch.utils.data import DataLoader
+
+
+class Utils:
+    @staticmethod
+    def seed_everything(seed: int):
+        torch.manual_seed(seed)
+        random.seed(seed)
+        np.random.seed(seed)
+        os.environ["PYTHONHASHSEED"] = str(seed)
+        if torch.cuda.is_available():
+            torch.cuda.manual_seed_all(seed)
+            torch.cuda.manual_seed(seed)
+            torch.backends.cudnn.deterministic = True
+
+    @staticmethod
+    def train_validation_split(X, y, z, preferences):
+        combined_data = list(zip(X, y, z))
+        random.shuffle(combined_data)
+        X, y, z = zip(*combined_data)
+        X_val = X[: int(preferences.validation_size * len(X))]
+        y_val = y[: int(preferences.validation_size * len(y))]
+        z_val = z[: int(preferences.validation_size * len(z))]
+        X = X[int(preferences.validation_size * len(X)) :]
+        y = y[int(preferences.validation_size * len(y)) :]
+        z = z[int(preferences.validation_size * len(z)) :]
+
+        return X, y, z, X_val, y_val, z_val
+
+    @staticmethod
+    def get_model(
+        dataset: str,
+        device: torch.device,
+        input_size: int = None,
+        output_size: int = None,
+    ) -> torch.nn.Module:
+        """This function returns the model to train.
+
+        Args:
+            dataset (str): the name of the dataset
+            device (torch.device): the device where the model will be trained
+
+        Raises:
+            ValueError: if the dataset is not supported
+
+        Returns:
+            torch.nn.Module: the model to train
+        """
+        if dataset == "celeba":
+            return CelebaNet()
+        elif dataset == "dutch":
+            return LinearClassificationNet(input_size=11, output_size=2)
+        elif dataset == "income":
+            return LinearClassificationNet(input_size=54, output_size=2)
+        else:
+            raise ValueError(f"Dataset {dataset} not supported")
+
+    @staticmethod
+    def get_optimizer(model, preferences):
+        if preferences.optimizer == "adam":
+            return torch.optim.Adam(
+                model.parameters(),
+                lr=preferences.learning_rate,
+            )
+        elif preferences.optimizer == "sgd":
+            return torch.optim.SGD(
+                model.parameters(),
+                lr=preferences.learning_rate,
+            )
+        elif preferences.optimizer == "adamW":
+            return torch.optim.AdamW(
+                model.parameters(),
+                lr=preferences.learning_rate,
+            )
+        else:
+            raise ValueError("Optimizer not recognized")
+
+    @staticmethod
+    def create_private_model(
+        model: torch.nn.Module,
+        preferences: Preferences,
+        original_optimizer,
+        train_loader,
+        delta: float,
+        noise_multiplier: float = 0,
+        accountant=None,
+    ) -> Tuple[GradSampleModule, DPOptimizer, DataLoader]:
+        """
+
+        Args:
+            model (torch.nn.Module): the model to wrap
+            epsilon (float): the target epsilon for the privacy budget
+            original_optimizer (_type_): the optimizer of the model before
+                wrapping it with Privacy Engine
+            train_loader (_type_): the train dataloader used to train the model
+            epochs (_type_): for how many epochs the model will be trained
+            delta (float): the delta for the privacy budget
+            MAX_GRAD_NORM (float): the clipping value for the gradients
+            batch_size (int): batch size
+
+        Returns:
+            Tuple[GradSampleModule, DPOptimizer, DataLoader]: the wrapped model,
+                the wrapped optimizer and the train dataloader
+        """
+        privacy_engine = PrivacyEngine(accountant="rdp")
+        if accountant:
+            privacy_engine.accountant = accountant
+
+        # We can wrap the model with Privacy Engine using the
+        # method .make_private(). This doesn't require you to
+        # specify a epsilon. In this case we need to specify a
+        # noise multiplier.
+        # make_private_with_epsilon() instead requires you to
+        # provide a target epsilon and a target delta. In this
+        # case you don't need to specify a noise multiplier.
+        if epsilon:
+            (
+                private_model,
+                optimizer,
+                train_loader,
+            ) = privacy_engine.make_private_with_epsilon(
+                module=model,
+                optimizer=original_optimizer,
+                data_loader=train_loader,
+                epochs=epochs,
+                target_epsilon=epsilon,
+                target_delta=delta,
+                max_grad_norm=MAX_GRAD_NORM,
+            )
+        else:
+            print(f"Create private model with noise multiplier {noise_multiplier}")
+            private_model, optimizer, train_loader = privacy_engine.make_private(
+                module=model,
+                optimizer=original_optimizer,
+                data_loader=train_loader,
+                noise_multiplier=noise_multiplier,
+                max_grad_norm=MAX_GRAD_NORM,
+            )
+            print(f"Created private model with noise {optimizer.noise_multiplier}")
+
+        return private_model, optimizer, train_loader, privacy_engine
