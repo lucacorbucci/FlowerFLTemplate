529a777e790eeeb012efbce598f9a5f3fe27af57
lucacorbucci <corbuccilu@gmail.com>
2025-09-03 09:39:35 +0200

    ✨ Added support to multiple local epochs

--- FLTemplate/Client/client.py
+++ FLTemplate/Client/client.py
@@ -7,7 +7,7 @@ from flwr.common import NDArrays, Scalar
 from Models.models import get_model
 from Training.training import test, train
 from Utils.preferences import Preferences
-from Utils.utils import get_params, set_params
+from Utils.utils import get_optimizer, get_params, set_params
 
 
 class FlowerClient(NumPyClient):
@@ -18,6 +18,7 @@ class FlowerClient(NumPyClient):
         self.valloader = valloader
         self.model = get_model(dataset=preferences.dataset_name)
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.preferences = preferences
 
     def fit(self, parameters, config):
         """This method trains the model using the parameters sent by the
@@ -28,10 +29,12 @@ class FlowerClient(NumPyClient):
         set_params(self.model, parameters)
 
         # Define the optimizer
+        optim = get_optimizer(self.model, self.preferences)
         optim = torch.optim.SGD(self.model.parameters(), lr=0.01, momentum=0.9)
 
         # do local training (call same function as centralised setting)
-        loss, accuracy = train(self.model, self.trainloader, optim, self.device)
+        for _ in range(self.preferences.num_epochs):
+            loss, accuracy = train(self.model, self.trainloader, optim, self.device)
 
         # return the model parameters to the server as well as extra info (number of training examples in this case)
         return get_params(self.model), len(self.trainloader), {"accuracy": accuracy, "loss": loss}
@@ -48,16 +51,3 @@ class FlowerClient(NumPyClient):
         return float(loss), len(self.valloader), {"accuracy": accuracy, "loss": loss}
 
 
-# Two auxhiliary functions to set and extract parameters of a model
-def set_params(model, parameters):
-    """Replace model parameters with those passed as `parameters`."""
-
-    params_dict = zip(model.state_dict().keys(), parameters)
-    state_dict = OrderedDict({k: torch.from_numpy(v) for k, v in params_dict})
-    # now replace the parameters
-    model.load_state_dict(state_dict, strict=True)
-
-
-def get_params(model):
-    """Extract model parameters as a list of NumPy arrays."""
-    return [val.cpu().numpy() for _, val in model.state_dict().items()]
