a50ee1e76d61860ac6a5c48e7c7a5715452bfe20
lucacorbucci <corbuccilu@gmail.com>
2025-09-03 16:37:03 +0200

    🐛 Tabular Dataset moved to Dutch dataset

--- FLTemplate/Datasets/dutch.py
+++ FLTemplate/Datasets/dutch.py
@@ -1,10 +1,49 @@
+from typing import Any
+
 import numpy as np
 import pandas as pd
 from Client.client import FlowerClient
-from Datasets.tabular_datasets import TabularDataset
 from sklearn.preprocessing import MinMaxScaler
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 from Utils.preferences import Preferences
+
+
+class DutchDataset(Dataset):
+    def __init__(self, x, z, y) -> None:
+        """
+        Initialize the custom dataset with x (features), z (sensitive values), and y (targets).
+
+        Args:
+        x (list of tensors): List of input feature tensors.
+        z (list): List of sensitive values.
+        y (list): List of target values.
+
+        """
+        self.samples = x
+        self.sensitive_features = z
+        self.targets = y
+        self.indexes = range(len(self.samples))
+
+    def __len__(self) -> int:
+        return len(self.samples)
+
+    def __getitem__(self, idx: int) -> tuple[Any, Any, Any]:
+        """
+        Get a single data point from the dataset.
+
+        Args:
+        idx (int): Index to retrieve the data point.
+
+        Returns:
+        sample (dict): A dictionary containing 'x', 'z', and 'y'.
+
+        """
+        x_sample = self.samples[idx]
+        z_sample = self.sensitive_features[idx]
+        y_sample = self.targets[idx]
+
+        return x_sample, z_sample, y_sample
+
 
 
 def get_dutch_scaler(
